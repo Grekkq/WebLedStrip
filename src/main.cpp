bff0c70b1608e5cdf7fcad080a7626f417a0ba78
Grekkq <grekkq97@gmail.com>
2020-03-20 13:06:31 +0100

    Bunch of includes and setting up WiFi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
+#include "SafeStorage.h"
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FS.h>
+#include <IRremoteESP8266.h>
+
+// WiFi config in SafeStorage.h Template:
+// #ifndef SAFESTORAGE_H
+// #define SAFESTORAGE_H
+// // WiFi config
+// const char *ssid = "Koparka Apokalipsy";
+// const char *password = "xd";
+// #endif
+
+void SetupWiFi(const char *ssid, const char *password) {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Trying to connect...");
+    }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("Connecting to WiFi:");
+    Serial.println(ssid);
+    SetupWiFi(ssid, password);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
